412f71f5d13a148d04923a71f911f7981ea2f733
Ryan Nielson <ryan.nielson@gmail.com>
2015-05-28 09:52:17 -0300

    Fix spacing

--- Assets/InputBinder/Editor/ButtonInputBindingPropertyDrawer.cs
+++ Assets/InputBinder/Editor/ButtonInputBindingPropertyDrawer.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 
 namespace RyanNielson.InputBinder
 {
-	[CustomPropertyDrawer(typeof(ButtonInputBinding))]
-	public class ButtonInputBindingPropertyDrawer : PropertyDrawer 
-	{
-		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) 
-		{
-			EditorGUI.PropertyField(position, property, label, true);
-		}
-		
-		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-		{
-			return EditorGUI.GetPropertyHeight(property);
-		}
-	}
+    [CustomPropertyDrawer(typeof(ButtonInputBinding))]
+    public class ButtonInputBindingPropertyDrawer : PropertyDrawer 
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) 
+        {
+            EditorGUI.PropertyField(position, property, label, true);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property);
+        }
+    }
 }
--- Assets/InputBinder/Editor/KeyInputBindingPropertyDrawer.cs
+++ Assets/InputBinder/Editor/KeyInputBindingPropertyDrawer.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 
 namespace RyanNielson.InputBinder
 {
-	[CustomPropertyDrawer(typeof(KeyInputBinding))]
-	public class KeyInputBindingPropertyDrawer : PropertyDrawer 
-	{
-		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) 
-		{
-			EditorGUI.PropertyField(position, property, label, true);
-		}
-		
-		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-		{
-			return EditorGUI.GetPropertyHeight(property);
-		}
-	}
+    [CustomPropertyDrawer(typeof(KeyInputBinding))]
+    public class KeyInputBindingPropertyDrawer : PropertyDrawer 
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) 
+        {
+            EditorGUI.PropertyField(position, property, label, true);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property);
+        }
+    }
 }
