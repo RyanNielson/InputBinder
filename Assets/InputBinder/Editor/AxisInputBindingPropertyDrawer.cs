ec877304eee2cc55bb63df2ec339b13066f29067
Ryan Nielson <ryan.nielson@gmail.com>
2015-05-28 19:09:53 -0300

    More experiments

--- Assets/InputBinder/Editor/AxisInputBindingPropertyDrawer.cs
+++ Assets/InputBinder/Editor/AxisInputBindingPropertyDrawer.cs
@@ -6,9 +6,39 @@ namespace RyanNielson.InputBinder
     [CustomPropertyDrawer(typeof(AxisInputBinding))]
     public class AxisInputBindingPropertyDrawer : PropertyDrawer 
     {
+        private bool foldedOut = false;
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) 
         {
-            EditorGUI.PropertyField(position, property, label, true);
+            SerializedProperty name = property.FindPropertyRelative("name");
+            SerializedProperty boundEvent = property.FindPropertyRelative("boundEvent");
+
+
+            foldedOut = EditorGUI.Foldout(position, foldedOut, "Neato");
+            if (foldedOut)
+            {
+                
+                Rect namePosition = position;
+                namePosition.y += 20;
+                namePosition.height = EditorGUI.GetPropertyHeight(name);
+
+                EditorGUI.PropertyField(namePosition, name, label, true);
+
+                Rect boundEventPosition = namePosition;
+                boundEventPosition.height = EditorGUI.GetPropertyHeight(boundEvent);
+                boundEventPosition.y += namePosition.height;
+                Debug.Log(namePosition);
+
+                EditorGUI.PropertyField(boundEventPosition, boundEvent, label, true);
+            }
+            //Debug.Log(position);
+            //Debug.Log(EditorGUI.GetPropertyHeight(name));
+            
+
+            //EditorGUI.PropertyField(position, property.FindPropertyRelative("name"));
+
+           // Debug.Log(EditorGUI.GetPropertyHeight(
+            //EditorGUI.PropertyField(position, property.FindPropertyRelative("boundEvent"));
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
